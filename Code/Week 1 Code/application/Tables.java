bbc1392371d9b878e6a7e45e85cd45b982fb6b58
Zaid Patel <zpatel24@uic.edu>
2020-02-10 16:22:54 -0600

    Week 1

---
+++ Code/Week 1 Code/application/Tables.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.util.PriorityQueue;
+import java.util.Vector;
+
+public class Tables extends Menu{
+    private Vector<Integer>v;
+	public int T1, T2, T3, T4, T5, T6, T7, T8, T9;
+	private boolean flag;
+	 public void get
+	
+    
+    public Tables(){
+    	v = new Vector<Integer>();
+    }
+	public void PrintMenu(){
+    		
+    }
+	public void CustomerArrives() {
+		if 
+	}
+    public void TableLogic() {
+    	PriorityQueue<Integer> t = new PriorityQueue<Integer>();
+    	if()
+    }
+	public static void main(String[] args) {
+		//System.out.println("hi");
+		Tables myTable = new Tables();
+		myTable.Items();
+		
+        
+	}
+
+}
