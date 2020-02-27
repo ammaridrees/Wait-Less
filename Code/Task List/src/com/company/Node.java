ee9596b97271bb87f0fce9e8cc5298003d8730bc
Bryan Gutierrez <bgutie8@uic.edu>
2020-02-26 20:49:00 -0600

    Task List Done & added GUI

---
+++ Code/Task List/src/com/company/Node.java
@@ -0,0 +1,7 @@
+package com.company;
+public class Node {
+    Task singleTask;
+    Node next;
+    Node tail;
+
+}
--- Code/Task List/src/com/company/User.java
+++ Code/Task List/src/com/company/User.java
@@ -1,4 +1,50 @@
 package com.company;
 
 public class User {
+    Queue userQueue;
+    static int countUsers = 0;
+    private int userID;
+    private String userPosition;
+    private String userName;
+
+    User(String name)
+    {
+        userQueue = new Queue();
+        userID = countUsers;
+        countUsers++;
+        userName = name;
+        userPosition = "Waiter";
+    }
+    User(Task newTask, String name)
+    {
+        userQueue = new Queue(newTask);
+        userID = countUsers;
+        countUsers++;
+        userName = name;
+        userPosition = "Waiter";
+    }
+    public int getUserID()
+    {
+        return this.userID;
+    }
+    public void setUserID(int ID)
+    {
+        this.userID = ID;
+    }
+    public String getUserPosition()
+    {
+        return this.userPosition;
+    }
+    public void setUserPosition(String position)
+    {
+        this.userPosition = position;
+    }
+    public String getUserName()
+    {
+        return this.userName;
+    }
+    public void setUserName(String name)
+    {
+        this.userName = name;
+    }
 }
