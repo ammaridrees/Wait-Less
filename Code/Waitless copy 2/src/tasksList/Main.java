2dbb98925ccd97b75fa60c2d6f703cbcb98a6d89
Bryan Gutierrez <bgutie8@uic.edu>
2020-04-09 11:31:46 -0500

    Task List update

--- Code/Waitless copy 2/src/tasksList/Main.java
+++ Code/Waitless copy 2/src/tasksList/Main.java
@@ -1,6 +1,8 @@
 package tasksList;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
@@ -12,25 +14,26 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import tableLayout.Tabledisplay;
 
 
 public class Main extends Application {
     private static ListView<String> TaskList;
-    Task newTask = new Task("Clean table");
-    Task newTask2 = new Task("Table 2");
-    Task newTask3 = new Task("Table 3");
-    Task newTask4 = new Task("Table 4");
+    //Task newTask = new Task("Clean table");
+    //Task newTask2 = new Task("Table 2");
+    //Task newTask3 = new Task("Table 3");
+    //Task newTask4 = new Task("Table 4");
     TaskController controller = new TaskController();
-    User newUser = new User("Maaz");
+    //User newUser = Tabledisplay.getUser();
     private Scene TaskListScene;
     private Button sendTask, moveTop, move, deleteTask, createTask;
-    private TextField sendTaskText, moveText, createUserTask;
+    private TextField sendTaskText, moveText, createUserTask, TableNumber;
     private VBox mainVBox;
     private HBox ButtonsBox;
+    public Stage mainStage;
     private BorderPane mainLayout;
-
     public static void main(String[] args) {
-
 
         User newUser2 = new User("Bryan");
         // User newUser = new User(newTask,"Maaz");
@@ -45,17 +48,24 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Task List");
+
+        mainStage = primaryStage;
+        mainStage.setTitle("Task List");
         TaskListScene = makeScene();
-        primaryStage.setScene(TaskListScene);
-        primaryStage.show();
+        mainStage.setScene(TaskListScene);
+        Platform.setImplicitExit(false);
+        mainStage.show();
+        primaryStage.setOnCloseRequest(t -> {
+            Platform.exit();
+           // System.exit(0);
+        });
 
         this.createTask.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                newUser.userQueue.add(new Task(createUserTask.getText()));
-                newUser.userQueue.printQueue();
-                Node temp = newUser.userQueue.head;
+                Tabledisplay.newUser.userQueue.add(new Task("Table: " + TableNumber.getText() + "--" + createUserTask.getText()));
+                Tabledisplay.newUser.userQueue.printQueue();
+                Node temp = Tabledisplay.newUser.userQueue.head;
                 //  while(temp != null)
                 //  {
                 TaskList.getItems().add(temp.tail.singleTask.getTaskCreator());
@@ -64,35 +74,50 @@ public class Main extends Application {
             }
         });
         this.deleteTask.setOnAction(new EventHandler<ActionEvent>() {
+
             @Override
             public void handle(ActionEvent event) {
-                Node temp = newUser.userQueue.head;
-                while (temp != null) {
-                    if (temp.singleTask.getTaskCreator().equals(TaskList.getSelectionModel().getSelectedItem())) {
-                        controller.deleteTask(newUser, temp.singleTask);
+                Node temp = Tabledisplay.newUser.userQueue.head;
 
-                    }
+                if(temp != null && TaskList.getSelectionModel().isEmpty())
+                {
+                    System.out.println("hi");
+                    controller.deleteTask(Tabledisplay.newUser);//,Tabledisplay.newUser.userQueue.head.singleTask);
                     temp = temp.next;
                 }
-                Node temp2 = newUser.userQueue.head;
+                    //if (temp != null && temp.singleTask.getTaskCreator().equals(TaskList.getSelectionModel().getSelectedItem())) {
+                      //  controller.deleteTask(newUser, newUser.userQueue.head.singleTask);
+                   // }
+                    while (temp != null && !TaskList.getSelectionModel().isEmpty()) {
+                        if (temp.singleTask.getTaskCreator().equals(TaskList.getSelectionModel().getSelectedItem())) {
+                            controller.deleteTask(Tabledisplay.newUser, temp.singleTask);
+                            break;
+                        }
+                        temp = temp.next;
+                    }
+
+
+                Node temp2 = Tabledisplay.newUser.userQueue.head;
                 TaskList.getItems().clear();
-                while (temp2 != null) {
+                while(temp2 != null)
+                {
                     TaskList.getItems().add(temp2.singleTask.getTaskCreator());
                     temp2 = temp2.next;
                 }
-                newUser.userQueue.printQueue();
+                Tabledisplay.newUser.userQueue.printQueue();
             }
         });
     }
 
     private Scene makeScene() {
-        newUser.userQueue.add(newTask);
-        newUser.userQueue.add(newTask2);
-        newUser.userQueue.add(newTask3);
-        newUser.userQueue.add(newTask4);
+        //newUser.userQueue.add(newTask);
+        //newUser.userQueue.add(newTask2);
+        //newUser.userQueue.add(newTask3);
+        //newUser.userQueue.add(newTask4);
+
         TaskList = new ListView<String>();
-        newUser.userQueue.printQueue();
-        Node temp = newUser.userQueue.head;
+        Tabledisplay.newUser.userQueue.printQueue();
+        Node temp = Tabledisplay.newUser.userQueue.head;
         while (temp != null) {
             TaskList.getItems().add(temp.singleTask.getTaskCreator());
             temp = temp.next;
@@ -100,13 +125,15 @@ public class Main extends Application {
         //TaskList.getItems().add(Integer.toString(newUser.userQueue.getLength()));
         TaskList.setEditable(false);
         createUserTask = new TextField();
+        TableNumber = new TextField();
         createTask = new Button("New Task");
         deleteTask = new Button("Delete");
         ButtonsBox = new HBox(10);
-        ButtonsBox.getChildren().addAll(createTask, createUserTask, deleteTask);
+        ButtonsBox.getChildren().addAll(createTask,TableNumber,createUserTask, deleteTask);
         mainVBox = new VBox(20);
         mainVBox.getChildren().addAll(TaskList, ButtonsBox);
         mainVBox.setAlignment(Pos.CENTER);
         return new Scene(mainVBox, 600, 600);
+
     }
 }
